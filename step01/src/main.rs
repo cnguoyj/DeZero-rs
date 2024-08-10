bef3da691c9758eb4afebe32588a0404b6196648
guoyanjun <guoyanjun.010@bytedance.com>
2024-08-10 10:48:48 +0800

    Using 'vec' to represent a matrix in Variable

--- step01/src/main.rs
+++ step01/src/main.rs
@@ -3,17 +3,18 @@ use ndarray::{Array};
 
 #[derive(Debug)]
 pub struct Variable<T> {
-    pub data:T,
+    pub data:Vec<T>,
 }
 
 impl <T> Variable<T> {
-    fn new(value:T) -> Self {
+    fn new(value:Vec<T>) -> Self {
         Variable { data:value }
     }
 }
 
 fn main() {
     let x = array![[1.0]];
-    let y = Variable::new(x);
+    println!("x {:?}",x);
+    let y = Variable::new(x.into_raw_vec());
     println!("y {:?}",y);
 }
--- step02/src/main.rs
+++ step02/src/main.rs
@@ -1,14 +1,13 @@
 use ndarray::prelude::*;
-use ndarray::{Array,Array1};
-use ndarray::OwnedRepr;
+use ndarray::{Array1};
 
 #[derive(Debug)]
 pub struct Variable<T> {
-    pub data:T,
+    pub data:Vec<T>,
 }
 
 impl <T> Variable<T> {
-    fn new(value:T) -> Self {
+    fn new(value:Vec<T>) -> Self {
         Variable { data:value }
     }
 }
@@ -25,20 +24,25 @@ trait Function<U> {
 
 struct Square {}
 
-impl Function<Array1<f64>> for Square {
-    fn forward(&self,x:&Variable<Array1<f64>>) -> Variable<Array1<f64>> {
-        let data = &x.data;
-        let x_squared = data.map(|data| data.powi(2));
-        Variable { data:x_squared }
+impl Function<f64> for Square {
+    fn forward(&self,x:&Variable<f64>) -> Variable<f64> {
+        let nd = Array1::from_vec(x.data.clone());
+        let x_squared = nd.map(|nd| nd.powi(2));
+        Variable { data:x_squared.into_raw_vec() }
     }
 }
 
 fn main() {
-    let x: ArrayBase<OwnedRepr<f64>, Dim<[usize; 2]>> = array![[10.0]];
-    let y = Variable::new(x);
+    let x = array![[10.0,20.0],[30.0,40.0]];
+    println!("x {:?}",x);
+    let y = Variable::new(x.into_raw_vec());
+    println!("y {:?}",y);
 
     let square_fun = Square {};
-    let var = square_fun.call(y);
+    let var = square_fun.call(&y);
 
-    //println!("var {:?}",var);
+    println!("var {:?}",var);
+
+    let arr = Array2::from_shape_vec((2, 2), var.data).unwrap();
+    println!("ndarray: {:?}", arr);
 }
