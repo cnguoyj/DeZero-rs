745039301d64827dd9ca00367005a57a9b648fc8
guoyanjun <guoyanjun.010@bytedance.com>
2024-09-01 18:04:39 +0800

    implement Automatic Differentiation in step07

--- step07/src/main.rs
+++ step07/src/main.rs
@@ -13,14 +13,14 @@ pub enum GradientFunction {
 }
 
 #[derive(Debug,Clone)]
-pub struct Variable<T> {
-    pub data:Rc<RefCell<Vec<T>>>,
-    pub grad:Rc<RefCell<Vec<T>>>,
+pub struct Variable {
+    pub data:Rc<RefCell<Vec<f64>>>,
+    pub grad:Rc<RefCell<Vec<f64>>>,
     pub creator:Option<Rc<RefCell<GradientFunction>>>,
 }
 
-impl <T> Variable<T> {
-    fn new(value:Rc<RefCell<Vec<T>>>) -> Rc<RefCell<Self>> {
+impl Variable {
+    fn new(value:Rc<RefCell<Vec<f64>>>) -> Rc<RefCell<Self>> {
         Rc::new(RefCell::new(
         Variable {
             data:value.clone(),
@@ -52,10 +52,58 @@ impl <T> Variable<T> {
         }
     }
     */
+
+      fn backward(&mut self) {
+        if let Some(creator_rc) = &self.creator {
+            let creator = creator_rc.borrow();
+            match &*creator {
+                GradientFunction::Square(square_ref) => {
+                    //println!("square_ref {:?}",square_ref);
+                    let square_rc = square_ref.borrow().upgrade();
+                    //println!("square_rc {:?}",square_rc);
+
+                     match square_rc {
+                        Some(ref square) => {
+                            let x = &square.input;
+                            let gy = self.grad.clone();
+                            //println!("square {:?}",square.clone());
+                            *x.borrow().grad.borrow_mut() = square.backward(&gy);
+                            x.borrow_mut().backward();
+                        },
+                        None => {
+                            //println!("Variable square_rc Creator None");
+                        },
+                    }
+                },
+                GradientFunction::Exp(exp_ref) => {
+                    let exp_rc = exp_ref.borrow().upgrade();
+                    //println!("exp_rc {:?}",exp_rc);
+
+                    match exp_rc {
+                       Some(ref exp) => {
+                           let x = &exp.input;
+                           let gy = self.grad.clone();
+                           //println!("Exp {:?}",exp.clone());
+                           *x.borrow().grad.borrow_mut() = exp.backward(&gy);
+                           x.borrow_mut().backward();
+                       },
+                       None => {
+                            //println!("Variable exp_rc Creator None");
+                       },
+                   }
+                }
+            }
+        }
+        /* 
+        else {
+            println!("No Creator");
+        }
+        */
+    }
 }
 
-trait Function<U> {
-    fn call(&mut self,input:&Rc<RefCell<Variable<U>>>) -> Rc<RefCell<Variable<U>>> {
+trait Function {
+    fn call(&mut self,input:&Rc<RefCell<Variable>>) -> Rc<RefCell<Variable>> {
         let output = self.forward(input);
         
         self.update_input(input);
@@ -67,19 +115,19 @@ trait Function<U> {
         output
     }
 
-    fn forward(&self,x:&Rc<RefCell<Variable<U>>>) -> Rc<RefCell<Variable<U>>> {
+    fn forward(&self,x:&Rc<RefCell<Variable>>) -> Rc<RefCell<Variable>> {
         unimplemented!("forward not Implemented");
     }
 
-    fn backward(&self,gy:&Rc<RefCell<Vec<U>>>) -> Vec<U> {
+    fn backward(&self,gy:&Rc<RefCell<Vec<f64>>>) -> Vec<f64> {
         unimplemented!("backward not Implemented");
     }
 
-    fn update_input(&mut self, input: &Rc<RefCell<Variable<U>>>) {
+    fn update_input(&mut self, input: &Rc<RefCell<Variable>>) {
         unimplemented!("update_input not Implemented");
     }
 
-    fn update_ouput(&mut self,output:&Rc<RefCell<Variable<U>>>) {
+    fn update_ouput(&mut self,output:&Rc<RefCell<Variable>>) {
         unimplemented!("update_ouput not Implemented");
     }
 
@@ -91,12 +139,12 @@ trait Function<U> {
 #[derive(Debug,Clone)]
 struct Square {
     selfclone:Option<Rc<Square>>,
-    input:Rc<RefCell<Variable<f64>>>,
-    output:Rc<RefCell<Variable<f64>>>,
+    input:Rc<RefCell<Variable>>,
+    output:Rc<RefCell<Variable>>,
 }
 
-impl Function<f64> for Square {
-    fn forward(&self,x:&Rc<RefCell<Variable<f64>>>) -> Rc<RefCell<Variable<f64>>> {
+impl Function for Square {
+    fn forward(&self,x:&Rc<RefCell<Variable>>) -> Rc<RefCell<Variable>> {
         let nd_x = Array1::from_vec(x.borrow().data.borrow().clone());
         let x_squared = nd_x.map(|nd| nd.powi(2));
         Rc::new(RefCell::new(
@@ -116,11 +164,11 @@ impl Function<f64> for Square {
         nd_gx.into_raw_vec()
     }
 
-    fn update_input(&mut self, input: &Rc<RefCell<Variable<f64>>>) {
+    fn update_input(&mut self, input: &Rc<RefCell<Variable>>) {
         self.input = input.clone();
     }
 
-    fn update_ouput(&mut self,output:&Rc<RefCell<Variable<f64>>>) {
+    fn update_ouput(&mut self,output:&Rc<RefCell<Variable>>) {
         self.output = output.clone();
     }
 
@@ -137,12 +185,12 @@ impl Function<f64> for Square {
 #[derive(Debug,Clone)]
 struct Exp {
     selfclone:Option<Rc<Exp>>,
-    input:Rc<RefCell<Variable<f64>>>,
-    output:Rc<RefCell<Variable<f64>>>,
+    input:Rc<RefCell<Variable>>,
+    output:Rc<RefCell<Variable>>,
 }
 
-impl Function<f64> for Exp {
-    fn forward(&self,x:&Rc<RefCell<Variable<f64>>>) -> Rc<RefCell<Variable<f64>>> {
+impl Function for Exp {
+    fn forward(&self,x:&Rc<RefCell<Variable>>) -> Rc<RefCell<Variable>> {
         let nd_x = Array1::from_vec(x.borrow().data.borrow().clone());
         let nd_exp = nd_x.map(|nd| nd.exp());
         Rc::new(RefCell::new(
@@ -163,11 +211,11 @@ impl Function<f64> for Exp {
         nd_gx.into_raw_vec()
     }
 
-    fn update_input(&mut self, input: &Rc<RefCell<Variable<f64>>>) {
+    fn update_input(&mut self, input: &Rc<RefCell<Variable>>) {
         self.input = input.clone();
     }
 
-    fn update_ouput(&mut self,output:&Rc<RefCell<Variable<f64>>>) {
+    fn update_ouput(&mut self,output:&Rc<RefCell<Variable>>) {
         self.output = output.clone();
     }
 
@@ -208,6 +256,7 @@ fn main() {
     let mut y = C.borrow_mut().call(&b);
     println!("y.data {:?}",y.borrow().data.borrow());
 
+    /* 
     let nd_y_grad = array![[1.0]];
     println!("nd_y_grad {:?}",nd_y_grad);
     let y_grad_vec:Vec<f64> = nd_y_grad.into_raw_vec();
@@ -249,4 +298,15 @@ fn main() {
     } else {
         println!("No creator");
     }
+    */
+
+    let nd_y_grad = array![[1.0]];
+    println!("nd_y_grad {:?}",nd_y_grad);
+    let y_grad_vec:Vec<f64> = nd_y_grad.into_raw_vec();
+    println!("y_grad_vec {:?}",y_grad_vec);
+
+    *y.borrow().grad.borrow_mut() = y_grad_vec;
+    println!("y {:?}",y);
+    y.borrow_mut().backward();
+    println!("x {:?}",x);
 }
